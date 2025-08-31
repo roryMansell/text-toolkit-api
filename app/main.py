c631cc4ccaf8c7be30fd1ac9e8b4a50217e9823b
roryMansell <rory.calum.mansell@outlook.com>
2025-08-31 12:31:27 +0100

    Add CI configuration, initial app structure, and update requirements

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routers import health, clean, sentiment, similarity
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Text Toolkit API", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # later lock down to your frontend origin
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(health.router)
 app.include_router(clean.router)
 app.include_router(sentiment.router)
